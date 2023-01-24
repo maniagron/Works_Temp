0425f94d7a2a5418aaebf27bf1bfca87561024bb
maniagron <59772484+maniagron@users.noreply.github.com>
2023-01-24 22:12:42 +0530

    Update task1_aws.sh

--- test project/fournine/Shell script/task1_aws.sh
+++ test project/fournine/Shell script/task1_aws.sh
@@ -13,4 +13,4 @@ aws ec2 run-instances --image-id ami-0c94855ba95c71c99 --count 1 --instance-type
 export AWS_REGION=eu-west-1
 
 # Spin up the second server
-aws ec2 run-instances --image-id ami-0c94855ba95c71c99 --count 1 --instance-type t2.micro --key-name my_key_pair --security-group-ids my_security_group --subnet-id my_subnet_id --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=Server2}]'
+aws ec2 run-instances --image-id ami-0c94855ba95c71c99 --count 1 --instance-type t2.micro --key-name my_key_pair --security-group-ids my_security_group --subnet-id my_subnet_id --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=Server2}]
